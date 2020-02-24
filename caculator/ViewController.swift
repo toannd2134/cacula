737ac2f0a16265126bedc657c76a2d44aeb4911c
Your Name <toannd213@.gmailcom>
2020-02-24 13:28:38 -0800

    first commit

--- caculator/ViewController.swift
+++ caculator/ViewController.swift
@@ -11,98 +11,102 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var myView: UILabel!
     @IBOutlet weak var myView2: UILabel!
-    var mang : [Double]  = []
+    var resurt : Double = 0
     var firstnumber : Double = 0
-    var SecondNumber :Double = 0
-    var paramaterOnSreen :Bool = false
-    var operation :Int = 0
+    var Secondnumber : Double = 0
+    var firstnumberS : String = ""
+    var SecondnumberS : String = ""
+    var curentParamater = Paramater.none
+     enum Paramater{
+    case division
+    case mutil
+    case minus
+    case add
+    case none
     
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-         
-         
     }
     
    
     @IBAction func ButonAct(_ sender: UIButton) {
-        if paramaterOnSreen == true{
-            paramaterOnSreen = false
-            myView2.text =  String(firstnumber)
-            myView.text = String(sender.tag)
-            SecondNumber = Double(myView.text ?? "") ?? 0
+        if curentParamater == .none   {
+            firstnumberS += String(sender.tag)
             
-         
-        }else if paramaterOnSreen == false {
-        
-            myView.text = myView.text! + String(sender.tag)
+            firstnumber = Double(firstnumberS)!
+            myView.text = String(firstnumber)
             
-            
+        } else if curentParamater != .none && Secondnumber != 0   {
+           SecondnumberS = String(sender.tag)
+            Secondnumber = Double(SecondnumberS)!
+            myView.text = SecondnumberS
+        }else if curentParamater != .none && Secondnumber == 0   {
+        myView.text = String(sender.tag)
+        Secondnumber = Double(myView.text!)!
         }
+        
     }
     
     @IBAction func ACTION(_ sender:UIButton) {
-        var chon = sender.tag
-        
-        
-         if chon == 10{
+        switch curentParamater {
+               case .division:
+                 firstnumber /= Secondnumber
+                 resurt = firstnumber
+                   myView.text = String(resurt)
+               case .mutil:
+                   firstnumber *= Secondnumber
+                   resurt = firstnumber
+                   myView.text = String(resurt)
+               case .minus :
+                   firstnumber -= Secondnumber
+                   resurt = firstnumber
+                   myView.text = String(resurt)
+               case .add :
+                   firstnumber += Secondnumber
+                   resurt = firstnumber
+                   myView.text = String(resurt)
+               default:
+                   print("none")
+               }
+        switch sender.tag {
+        case 10:
+            curentParamater = .none
             firstnumber = 0
-            SecondNumber = 0
-            myView.text = ""
-            myView2.text = ""
-        }else if chon == 11{
-            firstnumber = Double(myView.text!)!
-            myView.text =  "/"
-            paramaterOnSreen = true
-            operation = 11
-        }else if chon == 12 {
+            Secondnumber = 0
+            firstnumberS = ""
+            SecondnumberS = ""
+            resurt = 0
+            myView.text = String(resurt)
             
-            firstnumber = Double(myView.text!)!
-            mang.append(firstnumber)
-            myView.text = "*"
-            paramaterOnSreen = true
-            operation = 12
-           
-        }else if chon == 13 {
-           firstnumber = Double(myView.text!)!
-            mang.append(firstnumber)
-            myView.text = "-"
-            paramaterOnSreen = true
-             operation = 13
-           
-        }else if chon == 14 {
-            firstnumber = Double(myView.text!)!
-             mang.append(firstnumber)
-            myView.text = "+"
-            paramaterOnSreen = true
-           operation = 14
-        }else if chon == 15{
-            if operation == 11{
-                myView.text  = String(firstnumber/SecondNumber)
-                 myView2.text =  String(firstnumber)  + "/" + String(SecondNumber)
-                 
-            } else if operation == 12 {
-                myView.text  = String(firstnumber*SecondNumber)
-                myView2.text =  String(firstnumber)  + "*" + String(SecondNumber)
-            }else if operation == 13{
-                myView.text  = String(firstnumber-SecondNumber)
-                myView2.text =  String(firstnumber)  + "-" + String(SecondNumber)
-            }else if operation == 14 {
-                myView.text  = String(firstnumber+SecondNumber)
-                myView2.text =  String(firstnumber)  + "+" + String(SecondNumber)
-            }
-            
+        case 11:
+            curentParamater = .division
+        case 12:
+            curentParamater = .mutil
+        case 13 :
+            curentParamater = .minus
+        case 14 :
+            curentParamater = .add
+        case 15:
+            myView.text = String(resurt)
+        default:
+            curentParamater = .none
         }
+       
+
+    }
     
     
+    }
     
     /*case divide
     case multiply
     case minus
          case plus*/
         
-    }
     
     
     
-}
+    
+
 
